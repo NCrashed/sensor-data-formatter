1b8f4566862e4fca594a10db49e5234f0ebdb662
Anton Gushcha <ncrashed@gmail.com>
2019-05-12 15:16:53 +0300

    Make core widget for text conversion

--- app-front/src/App/Frontend.hs
+++ app-front/src/App/Frontend.hs
@@ -4,27 +4,32 @@ module App.Frontend(
 
 import App.Shared
 import Control.Monad.Fix
+import Data.Functor
+import Data.Text (Text)
 import Reflex.Dom
 
-frontend :: (MonadHold t m, PostBuild t m, DomBuilder t m, MonadFix m) => m ()
+type MonadFront t m = (MonadHold t m, PostBuild t m, DomBuilder t m, MonadFix m, DomBuilderSpace m ~ GhcjsDomSpace)
+
+frontend :: MonadFront t m => m ()
 frontend = do
   el "div" $ do
-    el "h4" $ text "First counter"
-    counter 0
-  el "div" $ do
-    el "h4" $ text "Second counter"
-    counter 42
+    el "h4" $ text "Лог с датами"
+    transformerWithTime
 
--- | Widget that contains from two buttons to increment and decrement value
-counter :: (MonadHold t m, PostBuild t m, DomBuilder t m, MonadFix m) => Int -> m ()
-counter v0 = elClass "div" "counter" $ mdo
-  incE <- button "+"
-  valD <- foldDyn ($) v0 $ leftmost [
-      (\v -> v+1) <$ incE
-    , (\v -> v-1) <$ decE
-    ]
-  dynText $ do
-    val <- valD
-    pure $ "Current value: " <> showt val
-  decE <- button "-"
+transformerWithTime :: MonadFront t m => m ()
+transformerWithTime = mdo
+  elClass "div" "row" $ do
+    txtInD <- elClass "div" "column" textAreaIn
+    elClass "div" "column" $ textAreaOut $ tag (current txtInD) btnE
+  btnE <- button "Обработать"
   pure ()
+
+textAreaIn :: MonadFront t m => m (Dynamic t Text)
+textAreaIn = do
+  ta <- textArea def
+  pure $ _textArea_value ta
+
+textAreaOut :: MonadFront t m => Event t Text -> m ()
+textAreaOut txtE = void $ textArea def {
+    _textAreaConfig_setValue = txtE
+  }
